ae178f1f1968978ffd9e4707d1e3779e236fb581
Wangdong Xu <w6xu@ucsd.edu>
2022-04-25 17:06:46 -0700

    callback

--- app/src/main/java/com/example/lab5/TodoListActivity.java
+++ app/src/main/java/com/example/lab5/TodoListActivity.java
@@ -18,8 +18,12 @@ public class TodoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
 
+        TodoListItemDao todoListItemDao = TodoDatabase.getSingleton(this).todoListItemDao();
+        List<TodoListItem> todoListItems = todoListItemDao.getAll();
+
         TodoListAdapter adapter = new TodoListAdapter();
         adapter.setHasStableIds(true);
+        adapter.setTodoListItems(todoListItems);
 
         recyclerView = findViewById(R.id.todo_items);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
